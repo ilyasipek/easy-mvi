105f7d771c608bda5e4b12074768d671de321b00
ilyasipek <ilyasipek1997@gmail.com>
2024-01-28 02:39:59 +0300

    Add collectAsUiEffectWithLifecycle extension

---
+++ mvi/src/main/java/com/pinkbang/mvi/MviFlowExtentions.kt
@@ -0,0 +1,35 @@
+package com.pinkbang.mvi
+
+import android.annotation.SuppressLint
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+
+@SuppressLint("ComposableNaming")
+@Composable
+fun <UiEffect> Flow<UiEffect>.collectAsUiEffectWithLifecycle(
+    lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
+    minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
+    context: CoroutineContext = EmptyCoroutineContext,
+    block: suspend CoroutineScope.(effect: UiEffect) -> Unit,
+) {
+    LaunchedEffect(this, lifecycleOwner) {
+        lifecycleOwner.lifecycle.repeatOnLifecycle(minActiveState) {
+            if (context == EmptyCoroutineContext) {
+                this@collectAsUiEffectWithLifecycle.collect { block.invoke(this, it) }
+            } else {
+                withContext(context) {
+                    this@collectAsUiEffectWithLifecycle.collect { block.invoke(this, it) }
+                }
+            }
+        }
+    }
+}
