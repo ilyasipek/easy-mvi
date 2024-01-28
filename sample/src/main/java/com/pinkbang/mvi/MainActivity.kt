cf783498e2ead136d42a7de12fef36f555e19222
ilyasipek <ilyasipek1997@gmail.com>
2024-01-29 01:31:05 +0300

    Replace default Greeting with HomeScreen

--- sample/src/main/java/com/pinkbang/mvi/MainActivity.kt
+++ sample/src/main/java/com/pinkbang/mvi/MainActivity.kt
@@ -3,13 +3,6 @@ package com.pinkbang.mvi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.pinkbang.mvi.ui.theme.MVITheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +10,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MVITheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+                HomeScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MVITheme {
-        Greeting("Android")
     }
 }
