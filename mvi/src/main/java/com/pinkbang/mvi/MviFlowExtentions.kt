98cc97fa87e219f9d7af05f8ee0d1fae6ec429cc
ilyasipek <ilyasipek1997@gmail.com>
2024-01-29 01:18:24 +0300

    Change collectAsUiEffectWithLifecycle's context to Dispatchers.Main.immediate

--- mvi/src/main/java/com/pinkbang/mvi/MviFlowExtentions.kt
+++ mvi/src/main/java/com/pinkbang/mvi/MviFlowExtentions.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 import kotlin.coroutines.CoroutineContext
@@ -18,16 +19,16 @@ import kotlin.coroutines.EmptyCoroutineContext
 fun <UiEffect> Flow<UiEffect>.collectAsUiEffectWithLifecycle(
     lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
     minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
-    context: CoroutineContext = EmptyCoroutineContext,
+    context: CoroutineContext = Dispatchers.Main.immediate,
     block: suspend CoroutineScope.(effect: UiEffect) -> Unit,
 ) {
     LaunchedEffect(this, lifecycleOwner) {
         lifecycleOwner.lifecycle.repeatOnLifecycle(minActiveState) {
             if (context == EmptyCoroutineContext) {
-                this@collectAsUiEffectWithLifecycle.collect { block.invoke(this, it) }
+                this@collectAsUiEffectWithLifecycle.collect { block(it) }
             } else {
                 withContext(context) {
-                    this@collectAsUiEffectWithLifecycle.collect { block.invoke(this, it) }
+                    this@collectAsUiEffectWithLifecycle.collect { block(it) }
                 }
             }
         }
